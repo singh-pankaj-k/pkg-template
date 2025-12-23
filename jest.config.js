d48c47cf74fde7e07df38acab178e46664392afb
Pankaj <pankajpriscilla@gmail.com>
2025-12-23 09:29:32 -0500

    Test error fixed.
    Signed-off-by: Pankaj <pankajpriscilla@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.base.json',
+      },
+    ],
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.ts'],
     coverageDirectory: 'coverage'
